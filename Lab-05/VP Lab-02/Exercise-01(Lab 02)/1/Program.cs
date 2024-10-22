aa9cfec4b6271f23025185f38bbdac9e78fdb1b8
Zahid-Zafar <233579@students.au.edu.pk>
2024-10-22 21:31:35 +0500

    Add files via upload

---
+++ Lab-05/VP Lab-02/Exercise-01(Lab 02)/1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Name: " + "Zahid Zafar");
+            Console.WriteLine("Registration No: " + "46374");
+            Console.WriteLine("Address: " + "Basti Malook Multan");
+        }
+    }
+}
